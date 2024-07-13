2357e25ef4b04427249715eee9d3c3b4517aed78
Pablo Correa <pcorrea@GXC.local>
2024-07-13 14:06:41 -0300

    Pantalla login y registrar

--- src/componentes/InicioSesion.js
+++ src/componentes/InicioSesion.js
@@ -1,5 +1,55 @@
+import { Helmet } from "react-helmet";
+import {
+  Header,
+  Titulo,
+  ContenedorHeader,
+  ContenedorBotones,
+} from "../elementos/Header";
+import Boton from "../elementos/Boton";
+import {
+  ContenedorFiltros,
+  Formulario,
+  Input,
+  InputGrande,
+  ContenedorBoton,
+} from "../elementos/ElementosDeFormulario";
+import { ReactComponent as SvgLogin } from "../imagenes/login.svg";
+import styled from "styled-components";
+
+const Svg = styled(SvgLogin)`
+  width: 100%;
+  max-height: 12.5rem;
+  margin-bottom: 1.25rem;
+`;
+
 function InicioSesion() {
-  return <h1>Inicio Sesion</h1>;
+  return (
+    <>
+      <Helmet>
+        <title>Iniciar Sesión</title>
+      </Helmet>
+
+      <Header>
+        <ContenedorHeader>
+          <Titulo>Iniciar Sesión</Titulo>
+          <div>
+            <Boton to="/crear-cuenta">Registrarse</Boton>
+          </div>
+        </ContenedorHeader>
+      </Header>
+
+      <Formulario>
+        <Svg />
+        <Input type="email" name="email" placeholder="Correo Electrónico" />
+        <Input type="password" name="password" placeholder="Contraseña" />
+        <ContenedorBoton>
+          <Boton as="button" type="submit" primario>
+            Iniciar Sesión
+          </Boton>
+        </ContenedorBoton>
+      </Formulario>
+    </>
+  );
 }
 
 export default InicioSesion;
--- src/componentes/RegistroUsuarios.js
+++ src/componentes/RegistroUsuarios.js
@@ -1,5 +1,53 @@
+import { Helmet } from "react-helmet";
+import { Header, Titulo, ContenedorHeader } from "../elementos/Header";
+import Boton from "../elementos/Boton";
+import {
+  Formulario,
+  Input,
+  ContenedorBoton,
+} from "../elementos/ElementosDeFormulario";
+import { ReactComponent as SvgLogin } from "../imagenes/registro.svg";
+import styled from "styled-components";
+
+const Svg = styled(SvgLogin)`
+  width: 100%;
+  max-height: 6.25rem;
+  margin-bottom: 1.25rem;
+`;
+
 function RegistroUsuarios() {
-  return <h1>Registro Usuarios</h1>;
+  return (
+    <>
+      <Helmet>
+        <title>Crear Cuenta</title>
+      </Helmet>
+
+      <Header>
+        <ContenedorHeader>
+          <Titulo>Crear Cuenta</Titulo>
+          <div>
+            <Boton to="/iniciar-sesion">Iniciar Sesión</Boton>
+          </div>
+        </ContenedorHeader>
+      </Header>
+
+      <Formulario>
+        <Svg />
+        <Input type="email" name="email" placeholder="Correo Electrónico" />
+        <Input type="password" name="password" placeholder="Contraseña" />
+        <Input
+          type="password"
+          name="password2"
+          placeholder="Repetir Contraseña"
+        />
+        <ContenedorBoton>
+          <Boton as="button" type="submit" primario>
+            Crear Cuenta
+          </Boton>
+        </ContenedorBoton>
+      </Formulario>
+    </>
+  );
 }
 
 export default RegistroUsuarios;
