4efeed84f20f96e00f059cb2f48182bf2cffec7d
Pablo Correa <pcorrea@GXC.local>
2024-07-15 20:26:13 -0300

    Logica de inicio de sesion

--- src/componentes/InicioSesion.js
+++ src/componentes/InicioSesion.js
@@ -8,6 +8,11 @@ import {
 } from "../elementos/ElementosDeFormulario";
 import { ReactComponent as SvgLogin } from "../imagenes/login.svg";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/firebaseConfig";
+import Alerta from "../elementos/Alerta";
 
 const Svg = styled(SvgLogin)`
   width: 100%;
@@ -16,6 +21,64 @@ const Svg = styled(SvgLogin)`
 `;
 
 function InicioSesion() {
+  const navigate = useNavigate();
+  const [correo, establecerCorreo] = useState("");
+  const [password, establecerPassword] = useState("");
+  const [estadoAlerta, cambiarEstadoAlerta] = useState(false);
+  const [alerta, cambiarAlerta] = useState({});
+
+  const handleChange = (e) => {
+    if (e.target.name === "email") {
+      establecerCorreo(e.target.value);
+    } else if (e.target.name === "password") {
+      establecerPassword(e.target.value);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    cambiarEstadoAlerta(false);
+    cambiarAlerta({});
+    const expresionRegular = /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/;
+    if (!expresionRegular.test(correo)) {
+      cambiarEstadoAlerta(true);
+      cambiarAlerta({
+        tipo: "error",
+        mensaje: "Por favor ingresa un correo electrónico valido",
+      });
+      return;
+    }
+    if (correo === "" || password === "") {
+      cambiarEstadoAlerta(true);
+      cambiarAlerta({
+        tipo: "error",
+        mensaje: "Por favor rellena todos los datos",
+      });
+      return;
+    }
+    try {
+      await signInWithEmailAndPassword(auth, correo, password);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      let mensaje;
+      switch (error.code) {
+        case "auth/invalid-credential":
+          mensaje = "Usuario o contraseña incorrectos";
+          break;
+        default:
+          mensaje = "Hubo un error al intentar iniciar sesión.";
+          break;
+      }
+
+      cambiarEstadoAlerta(true);
+      cambiarAlerta({
+        tipo: "error",
+        mensaje: mensaje,
+      });
+    }
+  };
+
   return (
     <>
       <Helmet>
@@ -31,16 +94,34 @@ function InicioSesion() {
         </ContenedorHeader>
       </Header>
 
-      <Formulario>
+      <Formulario onSubmit={handleSubmit}>
         <Svg />
-        <Input type="email" name="email" placeholder="Correo Electrónico" />
-        <Input type="password" name="password" placeholder="Contraseña" />
+        <Input
+          type="email"
+          name="email"
+          placeholder="Correo Electrónico"
+          value={correo}
+          onChange={handleChange}
+        />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Contraseña"
+          value={password}
+          onChange={handleChange}
+        />
         <ContenedorBoton>
           <Boton as="button" type="submit" $primario>
             Iniciar Sesión
           </Boton>
         </ContenedorBoton>
       </Formulario>
+      <Alerta
+        tipo={alerta.tipo}
+        mensaje={alerta.mensaje}
+        estadoAlerta={estadoAlerta}
+        cambiarEstadoAlerta={cambiarEstadoAlerta}
+      />
     </>
   );
 }
