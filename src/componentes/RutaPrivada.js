a559ae5d83d8bc697248e21c7e0a914fe6446cc8
Pablo Correa <pcorrea@GXC.local>
2024-07-15 21:21:51 -0300

    Seguridad de las pantallas

---
+++ src/componentes/RutaPrivada.js
@@ -0,0 +1,12 @@
+import { useAuth } from "../contextos/AuthContext";
+import { Navigate } from "react-router-dom";
+const RutaPrivada = ({ children }) => {
+  const { usuario } = useAuth();
+  if (usuario) {
+    return children;
+  } else {
+    return <Navigate replace to="/iniciar-sesion" />;
+  }
+};
+
+export default RutaPrivada;
--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import { Helmet } from "react-helmet";
 import favicon from "./imagenes/logo.png";
 import Fondo from "./elementos/Fondo";
 import { AuthProvider } from "./contextos/AuthContext";
+import RutaPrivada from "./componentes/RutaPrivada";
 
 WebFont.load({
   google: {
@@ -32,12 +33,41 @@ const Index = () => {
         <BrowserRouter>
           <Contenedor>
             <Routes>
-              <Route path="/" element={<App />} />
               <Route path="/iniciar-sesion" element={<InicioSesion />} />
               <Route path="/crear-cuenta" element={<RegistroUsuarios />} />
-              <Route path="/categorias" element={<GastosPorCategoria />} />
-              <Route path="/lista" element={<ListaDeGastos />} />
-              <Route path="/editar:id" element={<EditarGasto />} />
+
+              <Route
+                path="/categorias"
+                element={
+                  <RutaPrivada>
+                    <GastosPorCategoria />
+                  </RutaPrivada>
+                }
+              />
+              <Route
+                path="/lista"
+                element={
+                  <RutaPrivada>
+                    <ListaDeGastos />
+                  </RutaPrivada>
+                }
+              />
+              <Route
+                path="/editar/:id"
+                element={
+                  <RutaPrivada>
+                    <EditarGasto />
+                  </RutaPrivada>
+                }
+              />
+              <Route
+                path="/"
+                element={
+                  <RutaPrivada>
+                    <App />
+                  </RutaPrivada>
+                }
+              />
             </Routes>
           </Contenedor>
         </BrowserRouter>
